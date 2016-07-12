338dc126d5900ffbd5943ff9cae5f8c9de05e8d0
Brandon Rockhold <brandon.rockhold@gmail.com>
2016-07-12 14:29:17 -0500

    Formatting file with 90 char length to improve display of code within slides

--- misused-and-misunderstood-apis/src/main/java/com/bdorganization/easilymisunderstood/typeabuse/bad/RecurringAppointment.java
+++ misused-and-misunderstood-apis/src/main/java/com/bdorganization/easilymisunderstood/typeabuse/bad/RecurringAppointment.java
@@ -1,12 +1,15 @@
 package com.bdorganization.easilymisunderstood.typeabuse.bad;
 
 @SuppressWarnings({ "javadoc" })
-public interface RecurringAppointment
-{
+public interface RecurringAppointment {
     /**
-     * @return string representing the days of the week. For the day of the week that the recurrence is on, an 'X'
-     *         character will be present. For days of the week that there is no recurrence, a space character will be
-     *         present.
+     * @return a string representing the days of the week. For the day of the week that
+     *         the recurrence is on, an 'X' character will be present. For days of the
+     *         week that there is no recurrence, a space character will be present.
+     * 
+     *         Ex: " XXXXX " (Monday - Friday) 
+     *         
+     *         Ex: " X X XX" (Mon/Wed/Fri/Sat)
      */
     String getWeeklyRecurrence();
 }
